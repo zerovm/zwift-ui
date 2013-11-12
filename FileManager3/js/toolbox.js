b6ac339e60a225f81e811cb2e8b80d519fc93690
furball <furball@bigmir.net>
2013-11-12 18:52:47 +0200

    extended toolbox a bit

--- FileManager3/js/toolbox.js
+++ FileManager3/js/toolbox.js
@@ -6,23 +6,65 @@
 (function(){
 	"use strict";
 
-	var extensions = ["txt", "json", "html", "htm", "nexe", "py", "pdf", "doc", "ppt", "xls", "docx", "pptx", "xml", "zip", "mp3", "mp4", "gif", "jpg", "png", "css", "csv", "tar", "js", "lua", "c", "h", "jar", "xul", "psd", "avi"],
-		MIMETypes = ["text/plain", "application/json", "text/html", "text/html", "application/x-nexe", "text/x-python", "application/pdf", "application/msword", "application/vnd.ms-powerpoint", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/xml", "application/zip", "audio/mpeg", "audio/mp4", "image/gif", "image/jpeg", "image/png", "text/css", "text/csv", "application/x-tar", "text/javascript", "text/x-lua", "text/x-csrc", "text/x-chdr", "application/java-archive", "application/vnd.mozilla.xul+xml", "image/vnd.adobe.photoshop", "video/x-msvideo"];
+	var mimeObj = {},
+		extObj = {
+			"txt": {"mime": "text/plain", "isEditable": "true"},
+			"json": {"mime": "application/json", "isEditable": "true", "isExecutable": "true"},
+			"html": {"mime": "text/html", "isEditable": "true"},
+			"htm": {"mime": "text/html", "isEditable": "true"},
+			"nexe": {"mime": "application/x-nexe"},
+			"py": {"mime": "text/x-python", "isEditable": "true"},
+			"pdf": {"mime": "application/pdf"},
+			"doc": {"mime": "application/msword"},
+			"ppt": {"mime": "application/vnd.ms-powerpoint"},
+			"xls": {"mime": "application/vnd.ms-excel"},
+			"docx": {"mime": "application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+			"pptx": {"mime": "application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+			"xml": {"mime": "application/xml", "isEditable": "true"},
+			"zip": {"mime": "application/zip"},
+			"mp3": {"mime": "audio/mpeg"},
+			"mp4": {"mime": "audio/mp4"},
+			"gif": {"mime": "image/gif"},
+			"jpg": {"mime": "image/jpeg"},
+			"png": {"mime": "image/png"},
+			"css": {"mime": "text/css", "isEditable": "true"},
+			"csv": {"mime": "text/csv", "isEditable": "true"},
+			"tar": {"mime": "application/x-tar", isExecutable: "true"},
+			"js": {"mime": "text/javascript", "isEditable": "true"},
+			"lua": {"mime": "text/x-lua", "isEditable": "true"},
+			"c": {"mime": "text/x-csrc", "isEditable": "true"},
+			"h": {"mime": "text/x-chdr", "isEditable": "true"},
+			"jar": {"mime": "application/java-archive"},
+			"xul": {"mime": "application/vnd.mozilla.xul+xml", "isEditable": "true"},
+			"psd": {"mime": "image/vnd.adobe.photoshop"},
+			"avi": {"mime": "video/x-msvideo"}
+		};
 
-	function getExt(e){
-		var index = extensions.indexOf(e);
-		if(index !== -1){
-			return MIMETypes[index];
+	function getMIME(e){
+		var mime = extObj[e];
+		return mime && mime["mime"];
+	}
+
+	function getExt(m){
+		return mimeObj[m];
+	}
+
+	function isEditable(m){
+		var ext = getExt(m);
+		if(ext){
+			return extObj[ext["ext"]].isEditable;
 		}
 		return null;
 	}
-	function getMIMEType(e){
-		var index = MIMETypes.indexOf(e);
-		if(index !== -1){
-			return extensions[index];
+
+	function isExecutable(m){
+		var ext = getExt(m);
+		if(ext){
+			return extObj[ext["ext"]].isExecutable;
 		}
 		return null;
 	}
+
 	function checkParentClassName(el, className){
 		var topParentTag = "BODY";
 		while(el.tagName !== topParentTag){
@@ -34,12 +76,23 @@
 		return null;
 	}
 
+	Object.keys(extObj).forEach(function(ext){
+		var obj = extObj[ext],
+			mime = obj.mime;
+		mimeObj[mime] = {};
+		mimeObj[mime]["ext"] = ext;
+		mimeObj[mime]["isEditable"] = obj.isEditable;
+		mimeObj[mime]["isExecutable"] = obj.isExecutable;
+	});
+
 	if(!window.FileManager){
 		window.FileManager = {};
 	}
 	FileManager.toolbox = {
 		getExt: getExt,
-		getMIMEType: getMIMEType,
-		getParentByClassName: checkParentClassName
+		getMIMEType: getMIME,
+		getParentByClassName: checkParentClassName,
+		isEditable: isEditable,
+		isExecutable: isExecutable
 	};
 })();
