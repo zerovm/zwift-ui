352c62c2c4bebb2d56240f8782f76299d8c619b2
furball <furball@bigmir.net>
2013-11-21 23:33:21 +0200

    added submenu layout

--- FileManager3/js/item.js
+++ FileManager3/js/item.js
@@ -1,7 +1,10 @@
 (function(){
 	"use strict";
 
-	var selectedPath = null;
+	var selectedPath = null,
+		previousParent,
+		appearClass = "appear-submenu",
+		submenu = new Submenu();
 
 	function onclick(itemEl){
 		var name = itemEl.dataset.path;
@@ -28,7 +31,80 @@
 	}
 
 	function toggleMenu(e){
-		console.log(FileManager.toolbox.getParentByClassName(e, 'item'))
+		var parent = FileManager.toolbox.getParentByClassName(e, 'item');
+		if(previousParent === parent){
+			if(submenu.wrapper.classList.contains(appearClass)){
+				submenu.wrapper.classList.toggle(appearClass)
+			}
+		}else{
+			previousParent = parent;
+			submenu.wrapper.classList.remove(appearClass);
+			submenu.wrapper.parentNode && removeSubmenu();
+			submenu.setPath(parent.dataset.path);
+			appendSubmenu(parent);
+			setTimeout(function(){
+				submenu.wrapper.classList.add(appearClass);
+			}, 0);
+		}
+
+	}
+
+	function appendSubmenu(parentEl){
+		parentEl.parentNode.insertBefore(submenu.wrapper, parentEl.nextSibling);
+		setTimeout(function(){
+			submenu.wrapper.classList.add(appearClass);
+		}, 0);
+	}
+
+	function removeSubmenu(){
+		submenu.wrapper.classList.remove(appearClass);
+		submenu.wrapper.parentNode.removeChild(submenu.wrapper);
+	}
+
+	function Submenu(){
+		var button, wrapper, path,
+			buttonsClass = "submenu-items",
+			actionPrefix = "on",
+			handlers = {
+				onopen: function(e){
+				},
+				ondownload: function(e){
+				},
+				oncopy: function(e){
+				},
+				onmetadata: function(e){
+				},
+				ontype: function(e){
+				},
+				ondelete: function(e){
+				},
+				onexecute: function(e){
+				},
+				onedit: function(e){
+				}
+			};
+
+		this.setPath = function(p){
+			path = p;
+		};
+
+		this.wrapper = wrapper = document.createElement("div");
+		wrapper.className = "item submenu no-hover";
+
+		Object.keys(handlers).forEach(function(className){
+			className = className.replace(actionPrefix, "");
+			button = document.createElement("button");
+			button.className = className + " " + buttonsClass;
+			button.title = className;
+			button.dataset.action = className;
+			wrapper.appendChild(button);
+		});
+		wrapper.addEventListener("click", function(e){
+			var action = FileManager.toolbox.getParentByClassName(e.target, buttonsClass).dataset.action;
+			e.preventDefault();
+			e.stopPropagation();
+			action && handlers[actionPrefix + action]();
+		});
 	}
 
 	if(!window.FileManager){
--- FileManager3/js/uploadButtons.js
+++ FileManager3/js/uploadButtons.js
@@ -150,7 +150,7 @@ document.addEventListener("DOMContentLoaded", function(){
 			textEl, inputElement, button,
 			buttonWrapper = document.createElement("div"),
 			inputWrapper = document.createElement("div");
-		wrapper.className = "item upload-as";
+		wrapper.className = "item upload-as no-hover";
 		buttonWrapper.className = "button-wrapper";
 		inputWrapper.className = "input-wrapper";
 		form.className = "input-group";
